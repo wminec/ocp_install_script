ba776b943be199391b5742165c3690c7dc77fa59
wminec <wmienc@gmail.com>
2024-04-03 17:35:16 +0900

    change oc download.sh

--- oc/download-cli.sh
+++ oc/download-cli.sh
@@ -8,5 +8,5 @@ wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_RELEASE/open
 tar xfz $WORKDIR/openshift-client-linux-$OCP_RELEASE.tar.gz -C $WORKDIR/$OCP_RELEASE
 
 wget https://mirror.openshift.com/pub/openshift-v4/clients/ocp/$OCP_RELEASE/openshift-install-linux-$OCP_RELEASE.tar.gz -O $WORKDIR/$OCP_RELEASE/openshift-install-linux-$OCP_RELEASE.tar.gz
-tar xfz $WORKDIR/openshift-install-linux-4.11.43.tar.gz -C $WORKDIR/$OCP_RELEASE
+tar xfz $WORKDIR/openshift-install-linux-$OCP_RELEASE.tar.gz -C $WORKDIR/$OCP_RELEASE
 rm -rf $WORKDIR/$OCP_RELEASE/README.md
